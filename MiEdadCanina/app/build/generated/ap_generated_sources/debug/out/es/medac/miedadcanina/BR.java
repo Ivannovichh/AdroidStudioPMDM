ff88fd7950d8c68ad0ca97298c238713cd52895b
JotaMynds <ivansanjuar@gmail.com>
2026-01-22 10:01:56 +0100

    build app

---
+++ MiEdadCanina/app/build/generated/ap_generated_sources/debug/out/es/medac/miedadcanina/BR.java
@@ -0,0 +1,5 @@
+package es.medac.miedadcanina;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ MiEdadCanina/app/build/generated/data_binding_trigger/debug/es/medac/miedadcanina/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package es.medac.miedadcanina;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
